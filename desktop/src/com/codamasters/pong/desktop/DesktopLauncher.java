fabaa161f3f43fb7f610f9b005234c9ac3c2f4ee
julioxus <julioxus@gmail.com>
2015-05-03 17:48:27 +0200

    Proyecto recién creado

---
+++ desktop/src/com/codamasters/pong/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.codamasters.pong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.codamasters.pong.Pong;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pong(), config);
+	}
+}
