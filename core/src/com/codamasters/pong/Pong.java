4091b6f35e62eac88aed5142078132127cc0f492
julioxus <julioxus@gmail.com>
2015-05-03 22:16:52 +0200

    Trabajo de esta tarde

--- core/src/com/codamasters/pong/Pong.java
+++ core/src/com/codamasters/pong/Pong.java
@@ -1,27 +1,45 @@
 package com.codamasters.pong;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.codamasters.pong.screens.gameScreen;
 
-public class Pong extends ApplicationAdapter {
+public class Pong extends Game implements ApplicationListener {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new gameScreen());
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+
+	@Override
+	public void pause() {
+		super.pause();
+	}
+
+	@Override
+	public void resume() {
+		super.resume();
 	}
 }
--- desktop/src/com/codamasters/pong/desktop/DesktopLauncher.java
+++ desktop/src/com/codamasters/pong/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.codamasters.pong.Pong;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new Pong(), config);
 	}
 }
